e2e3ae348049b5cd093183d13dbc9e8df209e1f4
Charles Qiao <charles.qiao@eclipx.com>
2017-04-10 11:28:33 +1000

    minor fix

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ let step = 0;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setClearColor(0xeeeeee, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
@@ -70,14 +71,13 @@ camera.position.z = 30;
 camera.lookAt(scene.position);
 document.body.appendChild(renderer.domElement);
 
-
-window.addEventListener('resize', onResize, false);
-
 function onResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+window.addEventListener('resize', onResize, false);
 
 function renderScene() {
   stats.begin();
@@ -94,7 +94,6 @@ function renderScene() {
   requestAnimationFrame(renderScene);
   renderer.render(scene, camera);
 }
-
 
 // renderer.render(scene, camera);
 renderScene();
